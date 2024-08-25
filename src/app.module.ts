0226121e50ed02fc88d0ded0206c2e93f2528d15
Pihkaal <pihkaal@proton.me>
2024-08-25 18:31:50 +0200

    feat(env): load and validate env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
+import { EnvModule } from "./env/env.module";
+import { ConfigModule } from "@nestjs/config";
+import { envSchema } from "./env";
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      validate: (env) => envSchema.parse(env),
+      isGlobal: true,
+    }),
+    EnvModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,16 @@
 import { NestFactory } from "@nestjs/core";
+import { Logger } from "@nestjs/common";
 import { AppModule } from "~/app.module";
+import { EnvService } from "~/env/env.service";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const env = app.get(EnvService);
+  const port = env.get("PORT");
+
+  Logger.log(`Running on port ${port}`);
+  await app.listen(port);
 }
 
 bootstrap();
