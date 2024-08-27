c8ffa6fd6c49baa74ee77cdb19ecb190b98b71bc
Pihkaal <pihkaal@proton.me>
2024-08-27 15:20:10 +0200

    feat(caching): use redis

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { EnvModule } from "./env/env.module";
-import { ConfigModule } from "@nestjs/config";
+import { ConfigModule, ConfigService } from "@nestjs/config";
 import { envSchema } from "./env";
 import { HttpModule } from "@nestjs/axios";
 import { CacheModule } from "@nestjs/cache-manager";
+import * as redisStore from "cache-manager-redis-store";
 
 @Module({
   imports: [
@@ -13,8 +14,15 @@ import { CacheModule } from "@nestjs/cache-manager";
       validate: (env) => envSchema.parse(env),
       isGlobal: true,
     }),
-    CacheModule.register({
-      isGlobal: true,
+    CacheModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        store: redisStore,
+        host: configService.get("REDIS_HOST"),
+        port: configService.get("REDIS_PORT"),
+        isGlobal: true,
+      }),
     }),
     HttpModule,
     EnvModule,
--- src/env.ts
+++ src/env.ts
@@ -3,6 +3,8 @@ import { z } from "zod";
 export const envSchema = z.object({
   PORT: z.coerce.number().optional().default(3000),
   VOLTFM_USERNAME: z.string(),
+  REDIS_HOST: z.string().default("redis"),
+  REDIS_PORT: z.coerce.number().default(6379),
 });
 
 export type Env = z.infer<typeof envSchema>;
