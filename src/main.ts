11d10479393cddb6384d6e531ee810300f43328c
Pihkaal <pihkaal@proton.me>
2024-09-11 23:29:36 +0200

    feat(app): use helmet and enable cors

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,13 @@ import { NestFactory } from "@nestjs/core";
 import { Logger } from "@nestjs/common";
 import { AppModule } from "~/app.module";
 import { EnvService } from "~/env/env.service";
+import helmet from "helmet";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(helmet());
+  app.enableCors();
 
   const env = app.get(EnvService);
   const port = env.get("PORT");
